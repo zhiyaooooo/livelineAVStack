f0e27cdff8a495a9181355f408c9d3fcdfe1a00d
zhiyaooooo <143455830+zhiyaooooo@users.noreply.github.com>
2024-03-25 09:47:10 -0500

    Add files via upload

---
+++ src/client.jl
@@ -0,0 +1,113 @@
+struct VehicleCommand
+    steering_angle::Float64
+    velocity::Float64
+    controlled::Bool
+end
+
+function get_c()
+    c = 'x'
+    try
+        ret = ccall(:jl_tty_set_mode, Int32, (Ptr{Cvoid},Int32), stdin.handle, true)
+        ret == 0 || error("unable to switch to raw mode")
+        c = read(stdin, Char)
+        ccall(:jl_tty_set_mode, Int32, (Ptr{Cvoid},Int32), stdin.handle, false)
+    catch e
+    end
+    c
+end
+
+function keyboard_client(host::IPAddr=IPv4(0), port=4444; v_step = 1.0, s_step = π/10)
+    socket = Sockets.connect(host, port)
+    (peer_host, peer_port) = getpeername(socket)
+    msg = deserialize(socket) # Visualization info
+    @info msg
+
+    @async while isopen(socket)
+        sleep(0.001)
+        state_msg = deserialize(socket)
+        measurements = state_msg.measurements
+        num_cam = 0
+        num_imu = 0
+        num_gps = 0
+        num_gt = 0
+        for meas in measurements
+            if meas isa GroundTruthMeasurement
+                num_gt += 1
+            elseif meas isa CameraMeasurement
+                num_cam += 1
+            elseif meas isa IMUMeasurement
+                num_imu += 1
+            elseif meas isa GPSMeasurement
+                num_gps += 1
+            end
+        end
+        @info "Measurements received: $num_gt gt; $num_cam cam; $num_imu imu; $num_gps gps"
+    end
+    
+    target_velocity = 0.0
+    steering_angle = 0.0
+    controlled = true
+    
+    client_info_string = 
+        "********************
+      Keyboard Control (manual mode)
+      ********************
+        -Press 'q' at any time to terminate vehicle.
+        -Press 'i' to increase vehicle speed.
+        -Press 'k' to decrease vehicle speed.
+        -Press 'j' to increase steering angle (turn left).
+        -Press 'l' to decrease steering angle (turn right)."
+    @info client_info_string
+    while controlled && isopen(socket)
+        key = get_c()
+        if key == 'q'
+            # terminate vehicle
+            controlled = false
+            target_velocity = 0.0
+            steering_angle = 0.0
+            @info "Terminating Keyboard Client."
+        elseif key == 'i'
+            # increase target velocity
+            target_velocity += v_step
+            @info "Target velocity: $target_velocity"
+        elseif key == 'k'
+            # decrease forward force
+            target_velocity -= v_step
+            @info "Target velocity: $target_velocity"
+        elseif key == 'j'
+            # increase steering angle
+            steering_angle += s_step
+            @info "Target steering angle: $steering_angle"
+        elseif key == 'l'
+            # decrease steering angle
+            steering_angle -= s_step
+            @info "Target steering angle: $steering_angle"
+        end
+        cmd = (steering_angle, target_velocity, controlled)
+        serialize(socket, cmd)
+    end
+end
+
+function example_client(host::IPAddr=IPv4(0), port=4444)
+    socket = Sockets.connect(host, port)
+    map_segments = training_map()
+    (; chevy_base) = load_mechanism()
+
+    @async while isopen(socket)
+        state_msg = deserialize(socket)
+    end
+   
+    shutdown = false
+    persist = true
+    while isopen(socket)
+        position = state_msg.q[5:7]
+        @info position
+        if norm(position) >= 100
+            shutdown = true
+            persist = false
+        end
+        cmd = (0.0, 2.5, persist, shutdown)
+        serialize(socket, cmd) 
+    end
+
+end
---
+++ src/liveline.jl
@@ -0,0 +1,10 @@
+module liveline
+
+using VehicleSim
+using Sockets
+using Serialization
+
+include("client.jl")
+include("example_project.jl")
+
+end # module liveline
