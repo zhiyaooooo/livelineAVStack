c8a2362d27265ad06bfcba3798f1d63bccc250cc
mikuzhou <95454724+mikuzhou@users.noreply.github.com>
2024-04-16 15:18:29 -0500

    Update livelineAVStack.jl

--- src/livelineAVStack.jl
+++ src/livelineAVStack.jl
@@ -1,10 +1,16 @@
 module livelineAVStack
 
 using VehicleSim
+using LinearAlgebra
+using SparseArrays
 using Sockets
 using Serialization
+using StaticArrays
+using Ipopt
+using Symbolics 
 
 include("client.jl")
 include("example_project.jl")
+include("trajectory_functions.jl")
 
 end # module livelineAVStack
